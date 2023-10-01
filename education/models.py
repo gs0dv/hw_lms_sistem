a5099bcb3769dbe7fe08d1d719630c6d43c48b5e
gs0dv <gs0dv@main.ru>
2023-10-01 18:14:18 +0300

    update education\models.py: update model Course, added model Lesson

--- education/models.py
+++ education/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+NULLABLE = {
+    'blank': True,
+    'null': True
+}
+
 
 class Course(models.Model):
     title = models.CharField(max_length=300, verbose_name='название')
-    preview = models.ImageField(upload_to='education/', verbose_name='превью')
-    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='education/', verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
 
     def __str__(self):
         return f'{self.title}'
@@ -12,3 +17,17 @@ class Course(models.Model):
     class Meta:
         verbose_name = 'курс'
         verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=300, verbose_name='название')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    preview = models.ImageField(upload_to='education/', verbose_name='превью', **NULLABLE)
+    link = models.TextField(verbose_name='ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
