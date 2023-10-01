96837a89b39f18172598a561d2f6f073052ea7d4
gs0dv <gs0dv@main.ru>
2023-10-01 18:44:47 +0300

    update education\urls.py: added paths for LessonAPIViews

--- education/urls.py
+++ education/urls.py
@@ -1,7 +1,9 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from education.apps import EducationConfig
-from education.views import CourseViewSet
+from education.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDeleteAPIView
 
 app_name = EducationConfig.name
 
@@ -9,5 +11,10 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
+                  path('lessons/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+                  path('lessons/', LessonListAPIView.as_view(), name='lesson-list'),
+                  path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
+                  path('lessons/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+                  path('lessons/delete/<int:pk>/', LessonDeleteAPIView.as_view(), name='lesson-delete'),
 
-] + router.urls
+              ] + router.urls
