14a07f265ecdcc245cf97f6ce15aeee0f383f253
gs0dv <gs0dv@main.ru>
2023-10-01 18:09:39 +0300

    update education\models.py: added model Course

--- education/models.py
+++ education/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=300, verbose_name='название')
+    preview = models.ImageField(upload_to='education/', verbose_name='превью')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
