91f76cb62467aa73004e817652c5077e44b92d71
gs0dv <gs0dv@main.ru>
2023-10-03 20:01:00 +0300

    update education/serializers.py: added count_lessons_this_course in class CourseSerializer

--- education/serializers.py
+++ education/serializers.py
@@ -4,9 +4,14 @@ from education.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    count_lessons_this_course = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_count_lessons_this_course(self, course):
+        return Lesson.objects.filter(course=course).count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
