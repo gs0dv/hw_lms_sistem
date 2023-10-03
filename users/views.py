90cc8225769294d635e80b462f999476b4d15992
gs0dv <gs0dv@main.ru>
2023-10-03 20:43:10 +0300

    update files serializers.py, urls.py, views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserListSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserListSerializer
+    queryset = User.objects.all()
