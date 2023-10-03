527977d2045bf5d343285311533057ccd4df65b2
gs0dv <gs0dv@main.ru>
2023-10-03 20:46:52 +0300

    create new model "Payment" in education/models.py

--- education/models.py
+++ education/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 NULLABLE = {
@@ -33,3 +34,28 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
+
+
+class Payment(models.Model):
+    CASH = 'Наличные'
+    TRANSFER_TO_ACCOUNT = 'Перевод на счет'
+
+    PAYMENT_METHOD = (
+        (CASH, 'Наличные'),
+        (TRANSFER_TO_ACCOUNT, 'Перевод на счет'),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE,
+                             verbose_name='пользователь')
+    date_payment = models.DateField(verbose_name='дата оплаты')
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, **NULLABLE, verbose_name='оплаченный курс')
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, **NULLABLE, verbose_name='оплаченный урок')
+    payment_amount = models.PositiveIntegerField(verbose_name='сумма оплаты')
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD, verbose_name='периодичность рассылки')
+
+    def __str__(self):
+        return f'{self.user} - {self.payment_amount} ({self.paid_course if self.paid_course else self.paid_lesson})'
+
+    class Meta:
+        verbose_name = 'платеж'
+        verbose_name_plural = 'платежи'
